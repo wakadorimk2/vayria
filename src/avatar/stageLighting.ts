c8676538dcf4d921e62ce7eab1b527be0b20afaf
wakadori <wakadori.mk2@gmail.com>
2026-08-22 16:51:07 +0900

    feat: brighten stage background base colors (#37)

--- src/avatar/stageLighting.ts
+++ src/avatar/stageLighting.ts
@@ -1,5 +1,4 @@
 import {
-  Color,
   DirectionalLight,
   HemisphereLight,
   type Scene,
@@ -7,8 +6,6 @@ import {
 import { STAGE_PRESET } from './stagePreset';
 
 export function setupStageLighting(scene: Scene): void {
-  scene.background = new Color(STAGE_PRESET.backgroundColor);
-
   const hemisphere = STAGE_PRESET.lighting.hemisphere;
   scene.add(
     new HemisphereLight(
